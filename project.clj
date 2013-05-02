f6bbe95b9f2ce9855ab80c95304ed69486f9738b
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-02 22:18:01 +0200

    Clojar group.

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject cljss-units "0.2.0"
+(defproject jeremys/cljss-units "0.2.0"
   :description "Extensions adding units for cljss."
-
   :url "https://github.com/JeremS/cljss-units"
-
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cljss "0.1.0"]
                  [units "0.2.0"]]
