689b87f482a2fe9cb5d4f760f939c96fad3cd21a
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-03 14:34:26 +0200

    Forgot to update dependency.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cljss "0.1.0"]
+                 [cljss "0.2.0"]
                  [units "0.2.0"]]
 
   :profiles {:dev
