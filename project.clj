c7433f54163582f8f913a3fcfaaa0b69c3b15742
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-03 14:40:20 +0200

    The song remains the same...

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cljss-core "0.2.0"]
-                 [units "0.2.0"]]
+                 [jeremys/cljss-core "0.2.0"]
+                 [jeremys/units "0.2.0"]]
 
   :profiles {:dev
              {:dependencies [[midje "1.5.1"]]}})
