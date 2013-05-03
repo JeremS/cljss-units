caa9a04902c81392aebbdbf90c05014505d308ee
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-03 20:00:16 +0200

    Use of cljss.core v0.2.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [jeremys/cljss-core "0.2.0"]
+                 [jeremys/cljss-core "0.2.1"]
                  [jeremys/units "0.2.0"]]
 
   :profiles {:dev
