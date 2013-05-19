1b331777213bf311bdc779ec50c05ce5c7c6f5bb
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-19 21:04:03 +0200

    Use of units 0.2.2

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject jeremys/cljss-units "0.2.2"
+(defproject jeremys/cljss-units "0.2.2-SNAPSHOT"
   :description "Extensions adding units for cljss."
   :url "https://github.com/JeremS/cljss-units"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [jeremys/cljss-core "0.2.2"]
+                 [jeremys/cljss-core "0.3.0-SNAPSHOT"]
                  [jeremys/units "0.2.2"]]
 
   :profiles {:dev
