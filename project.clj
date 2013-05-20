6a8146600bcc5f303f451f7d4f419d24e44c242c
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-20 14:35:15 +0200

    _

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [jeremys/cljss-core "0.3.0-alpha1"]
-                 [jeremys/units "0.2.2"]]
+                 [jeremys/units "0.2.2-alpha1"]]
 
   :profiles {:dev
              {:dependencies [[midje "1.5.1"]]}})
